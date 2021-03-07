4172cd5c0020e94a0c9a31296b47d9b40abf17a6
MattiaSarti <nmattia1@gmail.com>
2021-03-07 16:43:06 +0100

    🍭

--- training/data.py
+++ training/data.py
@@ -3,12 +3,11 @@ Utilities for loading data.
 """
 
 
-from typing import List
-
-from spacy import load as spacy_load
 from torchtext.data import Field, Iterator
 from torchtext.data import batch as torchtext_batch
 from torchtext.datasets import IWSLT
+
+from transformer.training.preprocessing import Tokenizer
 
 
 class DatasetIterator(Iterator):
@@ -21,98 +20,83 @@ class DatasetIterator(Iterator):
 
         if self.train:
 
-            pass
+            def pool(data, random_shuffler) -> :
+                # TODO: data types
+                "."
+                pass
+
+            self.batches = pool(self.data(), self.random_shuffler)
 
         else:
 
             self.batches = []
-            # for each mini-batch untill all the dataset is covered:
-            for mini_batch in torchtext_batch(self.data(), self.batch_size,
-                                              self.batch_size_fn):
-                # appending ...
+            # for each mini-batch until all the dataset is covered:
+            for mini_batch in torchtext_batch(
+                    data=self.data(),
+                    batch_size=self.batch_size,
+                    batch_size_fn=self.batch_size_fn
+                    ):
+                # appending ...TODO: understand what
                 self.batches.append(
                     sorted(mini_batch, key=self.sort_key)
                 )
 
 
-class Tokenizer:
-    """
-    Source and target tokenizers.
-    """
-    def __init__(self, src_language: str = 'de', tgt_language: str = 'en')\
-            -> None:
-        super(Tokenizer, self).__init__()
+if __name__ == '__main__':
+    
+    # TODO: understand how to structure these instructions:
+    # max_sequence_length: int, min_vocabulary_counts: int
+    # self.max_sequence_length = max_sequence_length
+    # self.min_vocabulary_counts = min_vocabulary_counts
 
-        self.src_language_model = spacy_load(src_language)
-        self.tgt_language_model = spacy_load(tgt_language)
+    max_sequence_length = 100  # [number of tokens]
+    min_vocabulary_counts = 2
 
-        self.bos_token = "<s>"  # beginning of sequence
-        self.eos_token = "</s>"  # end of sequence
-        self.unk_token = "<unk>"  # unknown
-        self.padding_token = "<blank>"
+    tokenizer = Tokenizer(src_language='de', tgt_language='en')
 
-    def tokenize_src(self, text: str) -> List[str]:
-        "Tokenize source language text."
-        return [
-            token for token in self.src_language_model.tokenizer(text)
-        ]
-
-    def tokenize_tgt(self, text: str) -> List[str]:
-        "Tokenize target language text."
-        return [
-            token for token in self.tgt_language_model.tokenizer(text)
-        ]
-
-
-# TODO: understand how to structure these instructions:
-# max_sequence_length: int, min_vocabulary_counts: int
-# self.max_sequence_length = max_sequence_length
-# self.min_vocabulary_counts = min_vocabulary_counts
-max_sequence_length = 100
-min_vocabulary_counts = 2
-
-tokenizer = Tokenizer()
-
-# handlers for converting raw text into tokenized tensors:
-src_data_handler = Field(
-    tokenize=tokenizer.tokenize_src,
-    init_token=None,  # not required for source tokens
-    eos_token=None,  # not required for source tokens
-    pad_token=tokenizer.padding_token,
-    unk_token=tokenizer.unk_token,
-)
-tgt_data_handler = Field(
-    tokenize=tokenizer.tokenize_tgt,
-    init_token=tokenizer.bos_token,
-    eos_token=tokenizer.eos_token,
-    pad_token=tokenizer.padding_token,
-    unk_token=tokenizer.unk_token
-)
-
-# loading the samples while splitting them among training, validation and
-# test sets:
-training_samples, val_samples, test_samples = IWSLT.splits(
-    exts=('.de', '.en'),  # extensions to paths
-    fields=(src_data_handler, tgt_data_handler),  # tensor format converters
-    # choosing only samples for which filter_pred(sample) is True,
-    # corresponding to samples where both the source and the target
-    # sequences are shorter or equal to the maximum allowed length:
-    filter_pred=lambda x: (
-        (len(vars(x)['src']) <= max_sequence_length) and
-        (len(vars(x)['trg']) <= max_sequence_length)
+    # handlers for converting raw text into tokenized tensors:
+    src_data_handler = Field(
+        tokenize=tokenizer.tokenize_src,
+        init_token=None,  # not required for source tokens
+        eos_token=None,  # not required for source tokens
+        pad_token=tokenizer.padding_token,
+        unk_token=tokenizer.unk_token,
     )
-)
+    tgt_data_handler = Field(
+        tokenize=tokenizer.tokenize_tgt,
+        init_token=tokenizer.bos_token,
+        eos_token=tokenizer.eos_token,
+        pad_token=tokenizer.padding_token,
+        unk_token=tokenizer.unk_token
+    )
 
-# building source and target dictionaries from already tokenized training
-# samples:
-src_data_handler.build_vocsb(
-    training_samples.src,
-    min_freq=min_vocabulary_counts
-)
-tgt_data_handler.build_vocsb(
-    training_samples.trg,
-    min_freq=min_vocabulary_counts
-)
+    # loading the samples while splitting them among training, validation and
+    # test sets:
+    training_samples, val_samples, test_samples = IWSLT.splits(
+        exts=('.de', '.en'),
+        fields=(src_data_handler, tgt_data_handler),
+        # choosing only samples for which filter_pred(sample) is True,
+        # corresponding to samples where both the source and the target
+        # sequences are shorter or equal to the maximum allowed length:
+        filter_pred=lambda x: (
+            (len(vars(x)['src']) <= max_sequence_length) and
+            (len(vars(x)['trg']) <= max_sequence_length)
+            # TODO: adjust names of attributes ("MiniBatch" class ?)
+        )
+    )
+
+    # building source and target dictionaries from already tokenized training
+    # samples:
+    src_data_handler.build_vocab(
+        training_samples.src,
+        # TODO: adjust name of attribute ("MiniBatch" class ?)
+        min_freq=min_vocabulary_counts
+    )
+    tgt_data_handler.build_vocab(
+        training_samples.trg,
+        # TODO: adjust name of attribute ("MiniBatch" class ?)
+        min_freq=min_vocabulary_counts
+    )
 
 
 # TODO: set seed for deterministic, reproducible results:
---
+++ training/preprocessing.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from spacy import load as spacy_load
+
+
+class Tokenizer:
+    """
+    Source and target tokenizers.
+    """
+    def __init__(self, src_language: str = 'de', tgt_language: str = 'en')\
+            -> None:
+        super(Tokenizer, self).__init__()
+
+        self.src_language_model = spacy_load(src_language)
+        self.tgt_language_model = spacy_load(tgt_language)
+
+        self.bos_token = "<s>"  # beginning of sequence token
+        self.eos_token = "</s>"  # end of sequence token
+        self.unk_token = "<unk>"  # unknown token
+        self.padding_token = "<blank>"  # padding token
+
+    def tokenize_src(self, text: str) -> List[str]:
+        "Tokenize source language text."
+        return [
+            token for token in self.src_language_model.tokenizer(text)
+        ]
+
+    def tokenize_tgt(self, text: str) -> List[str]:
+        "Tokenize target language text."
+        return [
+            token for token in self.tgt_language_model.tokenizer(text)
+        ]
