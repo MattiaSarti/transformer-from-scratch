f7646fd47732ebec349972652aa314ca7489831a
MattiaSarti <nmattia1@gmail.com>
2021-06-06 01:10:56 +0200

    🍭 UPDATE - no unnecessary code and libraries 🍭

--- transformer/training_and_inference/preprocessing.py
+++ transformer/training_and_inference/preprocessing.py
@@ -3,36 +3,39 @@ Utilities for preprocessing, tokenizing and reconstructing text.
 """
 
 
-from typing import List
-
-from spacy import load as spacy_load
+raise NotImplementedError
 
 
-class Tokenizer:
-    """
-    Source and target tokenizers.
-    """
+# from typing import List
 
-    def __init__(self, src_language: str = 'de', tgt_language: str = 'en')\
-            -> None:
-        super(Tokenizer, self).__init__()
+# from spacy import load as spacy_load
 
-        self.src_language_model = spacy_load(src_language)
-        self.tgt_language_model = spacy_load(tgt_language)
 
-        self.bos_token = "<s>"  # beginning of sequence token
-        self.eos_token = "</s>"  # end of sequence token
-        self.unk_token = "<unk>"  # unknown token
-        self.padding_token = "<blank>"  # padding token
+# class Tokenizer:
+#     """
+#     Source and target tokenizers.
+#     """
 
-    def tokenize_src(self, text: str) -> List[str]:
-        """
-        Tokenize source language text.
-        """
-        return list(self.src_language_model.tokenizer(text))
+#     def __init__(self, src_language: str = 'de', tgt_language: str = 'en')\
+#             -> None:
+#         super(Tokenizer, self).__init__()
 
-    def tokenize_tgt(self, text: str) -> List[str]:
-        """
-        Tokenize target language text.
-        """
-        return list(self.tgt_language_model.tokenizer(text))
+#         self.src_language_model = spacy_load(src_language)
+#         self.tgt_language_model = spacy_load(tgt_language)
+
+#         self.bos_token = "<s>"  # beginning of sequence token
+#         self.eos_token = "</s>"  # end of sequence token
+#         self.unk_token = "<unk>"  # unknown token
+#         self.padding_token = "<blank>"  # padding token
+
+#     def tokenize_src(self, text: str) -> List[str]:
+#         """
+#         Tokenize source language text.
+#         """
+#         return list(self.src_language_model.tokenizer(text))
+
+#     def tokenize_tgt(self, text: str) -> List[str]:
+#         """
+#         Tokenize target language text.
+#         """
+#         return list(self.tgt_language_model.tokenizer(text))
